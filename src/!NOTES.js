e9c4056c1d300cf3703635ddcf148482e8cd0e55
Gucci Gangnam <diviner02hive@icloud.com>
2023-05-05 17:43:44 +0700

    end os day session. Notes.js added

---
+++ src/!NOTES.js
@@ -0,0 +1,7 @@
+// before oyu went for drinks----------------
+
+// You made a function that displays the game board.
+// The display gameboard function displays the game board and the nine divs and looks into the game board object and calls the symbols in the game board object to be displayed in the box Divs.
+// You haven't done any gameboard logic so you need to add event listeners to each box.
+// You already tried this outside of the display gameboard function but you realise that The devs are only created in the dorm once the display gameboard function is called. So you need to work that one out.
+// Maybe you could add the event listeners into the display gameboard function. (but then maybe call functions that are outside the display game board function. This might keep it more simple? I'm not sure? I'm going out for drinks now. I'll think about it later.)
